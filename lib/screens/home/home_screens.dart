44d7a777af65e1f74fa6a6ec700e1579d5e80ed8
unknown <leandrosenno@gmail.com>
2022-02-18 00:04:18 -0300

    Criação da appBar

---
+++ lib/screens/home/home_screens.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shop_app_ui/constants.dart';
+
+class Homescreens extends StatelessWidget {
+  const Homescreens({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/icons/Location.svg"),
+            const SizedBox(
+              width: defaultPadding / 2,
+            ),
+            Text(
+              "15/2 São Paulo",
+              style: Theme.of(context).textTheme.subtitle2,
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/Notification.svg"),
+          ),
+        ],
+      ),
+    );
+  }
+}
